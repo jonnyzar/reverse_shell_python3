199d7d392f7cc58a3a3cb73fbfe08a353de79f71
jonnyzar <yan.zaripov@gmail.com>
2020-06-13 12:14:34 -0400

    GNU Licence

--- rev_listener.py
+++ rev_listener.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#
 """
 This is server side or so called "listener".
 Once client is activated it connects to listener that
@@ -10,7 +25,7 @@ Parameter Description:
 c:client
 s:server
 """
-
+  
 
 import socket
 
