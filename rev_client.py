7ae17c604ed331eb9bbaae75b937f165bb3b2aee
jonnyzar <yan.zaripov@gmail.com>
2020-06-13 12:12:37 -0400

    Added client functionality to listen and get interrupted gracefully with Ctrl + C

--- rev_client.py
+++ rev_client.py
@@ -7,27 +7,64 @@ reverse shell.
 Once client is created, he connects to the listener, which controls 
 the reverse shell.
 """
-
-
-
 import socket
 
 
+class Connection_Mgr:
+    
+    def __init__(self):
+        self.c = None    
+    
+
+    def establish_conn(self,RHOST):
+        """
+        this function is to establish a connection to remote listener
+        it runs in loops until it finds connection
+        once found it returns connection object 
+        """
+        
+        while 1:            
+            #connection lifecycle loop
+            self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                           
+            try:
+                self.c.connect(RHOST)  
+                print (f"Successful connection at {RHOST}") 
+                return self.c
+                break
+            except ConnectionRefusedError:
+            #if connection refused then wait and retry
+                print ("Connection refused... lets try to reconnect")
+            except KeyboardInterrupt:
+                self.c.close()
+                print ("Excited gracefully")
+                break
+                 
+        
 
 def main():
-    RHOST = '127.0.0.1'
+    RADDR = '127.0.0.1'
     RPORT = 8888
-
-    #DGRAM : UDP, STREAM: DGRAM
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as c:
-        c.connect((RHOST,RPORT)) 
-        c.sendall(b'BIG HI') 
-        resp = c.recv(1024)
+    RHOST = (RADDR,RPORT)
+    client_mgr = Connection_Mgr()
+    c = None
     
-    
-    print ('Received: ', repr(data)) 
+    while 1:
+        #client lifecycle loop     
+        #establish connection with RHOST
+        try:            
+            c = client_mgr.establish_conn(RHOST) 
+            if c != None:                    
+                c.sendall(b'BIG HI') 
+                resp = c.recv(1024)   
+                print ('Received: ', repr(data)) 
+        except KeyboardInterrupt:
+            if c != None:
+                c.close()
+            print ("Excited gracefully")
+            break
+            
  
-    
     return 0
 
 if __name__ == '__main__':
--- rev_listener.py
+++ rev_listener.py
@@ -5,6 +5,10 @@
 This is server side or so called "listener".
 Once client is activated it connects to listener that
 can issue commands to client.
+
+Parameter Description:
+c:client
+s:server
 """
 
 
@@ -16,15 +20,21 @@ def main():
     LPORT = 8888
 
     
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((LHOST,LPORT))
         s.listen(5) #server can work with maximum 5 clients
-        
-        
-        #result = s.recv(1024)
-        #print ('Message: ',   result.decode('utf-8'))
-  
-    
+        #loop for adding clients on connection
+        while 1:
+            try:
+                c, addr = s.accept()
+            except KeyboardInterrupt:
+                s.close()
+                break
+            else:
+                #get client message
+                c_message = c.recv(1024) 
+                print ('Message: ',   c_message.decode('utf-8'))   
+            
     return 0
 
 if __name__ == '__main__':
